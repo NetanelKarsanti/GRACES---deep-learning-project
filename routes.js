71a0f250720c66cf7ef9d659ab9bea49f97f471b
NetanelKarsanti <140518138+NetanelKarsanti@users.noreply.github.com>
2023-07-25 17:12:16 +0300

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        path: "/",
+        component: Home,
+        exact: true,
+    },
+    {
+        path: "/about",
+        component: About,
+        exact: true,
+    },
+    {
+        path: "/contact",
+        component: Contact,
+        exact: true,
+    },
+    {
+        path: "/secret",
+        component: Secret,
+        exact: true,
+    },
